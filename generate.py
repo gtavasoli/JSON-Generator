24e904e5220a7597902fee73a9e14c350f01e7e7
Gholamhossein Tavasoli <ghht.com@gmail.com>
2019-07-25 10:11:05 +0430

    Bug fix
    Bug fix
    Improve repeat for list

--- generate.py
+++ generate.py
@@ -11,16 +11,32 @@ def generate(template):
         return obj
 
     if type(template) == list:
-        s = template[0].find('(') + 1
-        e = template[0].find(')')
-        rng = template[0][s:e].replace(' ', '').split(',')
-        if len(rng) > 1:
-            repeat = random.randint(int(rng[0]), int(rng[1]))
-        else:
-            repeat = int(rng[0])
         lst = []
-        for i in range(0, repeat):
-            lst.append(generate(template[1]))
+        it = iter(template)
+        while True:
+            r = next(it, None)
+            if r is None:
+                break
+            if type(r) == str:
+                regex = r"^\{\{repeat\(\s*\d+\s*,{0,1}\s*\d*\s*\)\}\}$"
+                matches = re.match(regex, r)
+                if matches is not None:  # Repeat
+                    s = template[0].find('(') + 1
+                    e = template[0].find(')')
+                    rng = template[0][s:e].replace(' ', '').split(',')
+                    if len(rng) > 1:
+                        repeat = random.randint(int(rng[0]), int(rng[1]))
+                    else:
+                        repeat = int(rng[0])
+                    r = next(it, None)  # Repeat it
+                    if r is None:
+                        break
+                    for i in range(0, repeat):
+                        lst.append(generate(r))
+                else:
+                    lst.append(generate(r))
+            else:
+                lst.append(generate(r))
         return lst
 
     if type(template) == str:
--- main.py
+++ main.py
@@ -1,39 +1,9 @@
 import json
-import unittest
 
 from generate import generate
 
-template = [
-    '{{repeat(1)}}',
-    {
-        "_id": '{{object_id()}}',
-        'guid': '{{guid()}}',
-        'isActive': '{{bool()}}',
-        'eyeColor': '{{choice(["blue", "brown", "green"])}}',
-        'balance': '{{floating(1000, 4000, 2)}}',
-        'picture': 'http://placehold.it/32x32',
-        'age': '{{integer(20, 30)}}',
-        'name': '{{name()}}',
-        'city': '{{city()}}',
-        'state': '{{state()}}',
-        'gender': '{{gender()}}',
-        'company': '{{company().upper()}}',
-        'email': '{{email()}}',
-        'phone': '{{phone()}}',
-        'latitude': '{{floating(-90.000001, 90)}}',
-        'longitude': '{{floating(-180.000001, 180)}}',
-        'tags': [
-            '{{repeat(3, 7)}}',
-            '{{last_name()}}'
-        ],
-        'friends': [
-            '{{repeat(3)}}',
-            {
-                'id': '{{integer()}}',
-                'name': '{{name()}}'
-            }
-        ]
-    }]
-
 if __name__ == '__main__':
-    print(json.dumps(generate(template), indent=4, sort_keys=True))
+    with open('./templates/sample_3.json', 'r') as f:
+        s = f.read()
+        template = json.loads(s)
+        print(json.dumps(generate(template), indent=4, sort_keys=True))
